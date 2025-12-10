5196e71f96ad833f70c85fe65ccd3782030e50f2
sobryan <stobryan@gmail.com>
2025-12-10 00:20:28 -0600

    and other things

---
+++ src/main/java/com/example/masterbatcher/MasterbatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.masterbatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterbatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterbatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/masterbatcher/MasterbatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masterbatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterbatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
